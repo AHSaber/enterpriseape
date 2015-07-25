bbe5fc5c2af6059205a1805db6ddbedf72094c84
Alex Saber <asaber@caltropcorp.com>
2015-07-25 00:10:34 -0700

    |

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :invoices
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get 'invoice#index'
   # You can have the root of your site routed with "root"
   root 'invoice#index'
 
